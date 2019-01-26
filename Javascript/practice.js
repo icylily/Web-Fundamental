089781c6e2183107b0c8c0a73ef1d2bcfe924509
icylily <icylily0116@gmail.com>
2019-01-25 16:12:12 -0800

    Add The Internet page

--- Javascript/practice.js
+++ Javascript/practice.js
@@ -1,4 +1,19 @@
-for (var i=0;i<23;i++)
+function insertat(arr,val,idx)
 {
-    console.log(i);
+    if (idx>(arr.length-1)){
+        return false;
+    }
+    arr.push(arr[arr.length-1]);
+    for(var i= (arr.length-2);i>=idx;i--){
+        arr[i+1]=arr[i];
+        
+    }
+    arr[idx]=val;
+    return true;
 }
+
+arr=[1,2,3,4];
+console.log(insertat(arr,0,2));
+console.log(arr);
+
+
